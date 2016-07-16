483acb80b12fb8bc6b1a8827cf32cd2d454baae5
Anfelroth <andresfboco@hotmail.com>
2016-07-16 16:22:14 -0500

    added mongodb cartrigde2

--- meteorshim.js
+++ meteorshim.js
@@ -4,7 +4,7 @@
 var APP_DNS = process.env.OPENSHIFT_APP_DNS_ALIAS || process.env.OPENSHIFT_APP_DNS;
 
 process.env.ROOT_URL = "http://" + APP_DNS || "http://localhost";
-process.env.MONGO_URL = "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/";
+process.env.MONGO_URL = "mongodb://admin:EQ9HszDk5-bQ@127.4.226.2:27017/cliente";
 // process.env.MAIL_URL = "smtp://email:password@smtp.gmail.com:465";
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
