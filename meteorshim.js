05771ae44942ef845dbc9ab114c144931e721d14
Anfelroth <andresfboco@hotmail.com>
2016-07-16 19:49:44 -0500

    added mongodb env3

--- meteorshim.js
+++ meteorshim.js
@@ -4,7 +4,7 @@
 var APP_DNS = process.env.OPENSHIFT_APP_DNS_ALIAS || process.env.OPENSHIFT_APP_DNS;
 
 process.env.ROOT_URL = "https://" + APP_DNS || "http://localhost";
-process.env.MONGO_URL = "mongodb://admin:EQ9HszDk5-bQ@127.0.0.1:27018 /cliente";
+process.env.MONGO_URL = "mongodb://admin:EQ9HszDk5-bQ@127.0.0.1:27018/cliente";
 // process.env.MAIL_URL = "smtp://email:password@smtp.gmail.com:465";
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
